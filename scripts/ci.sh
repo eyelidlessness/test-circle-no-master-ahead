3efd4e382dba1613bd2d6491c96891521f640656
eyelidlessness <gnosis@gmail.com>
2019-11-20 18:52:20 -0600

    PFFT

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+if [[ "$DEPLOYMENT_BRANCH" == "" ]]; then
+    echo "Environment variable \$DEPLOYMENT_BRANCH must be configured"
+    exit 1
+fi
+
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-CHANGES=$(git rev-list --left-right --count master...$CURRENT_BRANCH --count)
+CHANGES=$(git rev-list --left-right --count $DEPLOYMENT_BRANCH...$CURRENT_BRANCH --count)
 BEHIND_ZERO=$(echo $CHANGES | grep '^0\s')
 
 if [[ "$BEHIND_ZERO" == "" ]]; then
-  echo "Cannot merge behind master"
-  exit 1
+    echo "Unable to merge when behind master"
+    exit 1
 fi
 
 exit 0
